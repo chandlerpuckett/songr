c2b4dfd9a949624579859cefc42e7799026f10bb
Chandler Puckett <chandler.puckett1@gmail.com>
2020-09-28 11:39:52 -0700

    if else statements with thymeleaf

--- src/main/java/com/chandlerpuckett/songr/HomeController.java
+++ src/main/java/com/chandlerpuckett/songr/HomeController.java
@@ -1,5 +1,6 @@
 package com.chandlerpuckett.songr;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 
@@ -7,8 +8,26 @@ import org.springframework.stereotype.Controller;
 public class HomeController {
 
     @GetMapping("/")                            // in js: app.get("/)
-    public String showHome () {
+    public String showHome(){
         return "index";                          // filename for thymeleaf file for render --> ejs
+    }
+
+//    http://localhost:8080/greeting?person=%22chandler%22&lastSeen=0&message=%22good%20to%20see%20you%22
+    @GetMapping("/greeting")
+    public String sayHello(Model mPotato, String person, Integer lastSeen, String message){
+
+        System.out.println(String.format(
+                "Message from %s :" +
+                "it's been %d days since i saw you, %s",
+                person,
+                lastSeen,
+                message));
+
+        mPotato.addAttribute("person",person);
+        mPotato.addAttribute("days",lastSeen);
+        mPotato.addAttribute("message",message);
+
+        return "hello";
     }
 
 
